82a7625407593a3bef06c7de7c3c0950007246d8
yesid <yesid.manjarres@aule.co>
2022-08-19 16:56:41 -0500

    ...

--- src/components/workspace/subject/Subject.jsx
+++ src/components/workspace/subject/Subject.jsx
@@ -1,87 +1,87 @@
 import { Fragment } from 'react';
 import classNames from 'classnames';
 import {
-    generatePath,
-    NavLink,
-    Outlet,
-    useParams
+  generatePath,
+  NavLink,
+  Outlet,
+  useParams
 } from 'react-router-dom';
 
 import routes from '../../../constants/routes';
 
 function Subject() {
-    const { subjectId } = useParams();
+  const { subjectId } = useParams();
 
-    const renderNavigation = () => {
-        let itemClassNames = ({ isActive }) => classNames(
-            "main-navigation__item",
-            isActive && "item-active"
-        );
+  const renderNavigation = () => {
+    let itemClassNames = ({ isActive }) => classNames(
+      "main-navigation__item",
+      isActive && "item-active"
+    );
 
-        return (
-            <div className="main-navigation">
-                <NavLink
-                    className={itemClassNames}
-                    to={generatePath(
-                        routes.workspaceSubjectActivities,
-                        { subjectId }
-                    )}
-                >
-                    Activities
-                </NavLink>
-                <NavLink
-                    className={itemClassNames}
-                    to={generatePath(
-                        routes.workspaceSubjectConversation,
-                        { subjectId }
-                    )}
-                >
-                    Conversation
-                </NavLink>
-                <NavLink
-                    className={itemClassNames}
-                    to={generatePath(
-                        routes.workspaceSubjectAttendance,
-                        { subjectId }
-                    )}
-                >
-                    Attendance
-                </NavLink>
-                <NavLink
-                    className={itemClassNames}
-                    to={generatePath(
-                        routes.workspaceSubjectUsers,
-                        { subjectId }
-                    )}
-                >
-                    Users
-                </NavLink>
-                <NavLink
-                    className={itemClassNames}
-                    to={generatePath(
-                        routes.workspaceSubjectSettings,
-                        { subjectId }
-                    )}
-                >
-                    Settings
-                </NavLink>
-            </div>
-        );
-    };
+    return (
+      <div className="main-navigation">
+        <NavLink
+          className={itemClassNames}
+          to={generatePath(
+            routes.workspaceSubjectActivities,
+            { subjectId }
+          )}
+        >
+          Activities
+        </NavLink>
+        <NavLink
+          className={itemClassNames}
+          to={generatePath(
+            routes.workspaceSubjectConversation,
+            { subjectId }
+          )}
+        >
+          Conversation
+        </NavLink>
+        <NavLink
+          className={itemClassNames}
+          to={generatePath(
+            routes.workspaceSubjectAttendance,
+            { subjectId }
+          )}
+        >
+          Attendance
+        </NavLink>
+        <NavLink
+          className={itemClassNames}
+          to={generatePath(
+            routes.workspaceSubjectUsers,
+            { subjectId }
+          )}
+        >
+          Users
+        </NavLink>
+        <NavLink
+          className={itemClassNames}
+          to={generatePath(
+            routes.workspaceSubjectSettings,
+            { subjectId }
+          )}
+        >
+          Settings
+        </NavLink>
+      </div>
+    );
+  };
 
-    const render = () => {
-        return (
-            <Fragment>
-                <h1 className="main-content__header">{subjectId}</h1>
-                {renderNavigation()}
-                <div className="main-content__body">
-                    <Outlet />
-                </div>
-            </Fragment>
-        );
-    };
+  const render = () => {
+    return (
+      <Fragment>
+        <h1 className="main-content__header">{subjectId}</h1>
+        {renderNavigation()}
+        <div className="main-content__body">
+          <Outlet />
+        </div>
+      </Fragment>
+    );
+  };
 
-    return render();
+  return render();
 }
 
 export default Subject;
